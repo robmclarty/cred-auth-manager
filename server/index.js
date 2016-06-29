20170ef61510098897897cb7ac570dfe75ac8951
Robert McLarty <rob.m@myplanet.com>
2016-06-28 20:46:11 -0400

    start setting up usage of cred package

--- server/index.js
+++ server/index.js
@@ -6,9 +6,56 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cors = require('cors');
 const fs = require('fs');
+const cred = require('cred');
 const config = require('../config/server');
 
 const app = express();
+
+app.set('name', config.appName);
+app.set('issuer', config.issuer);
+
+// Authentication
+const auth = cred({
+  issuer: app.get('issuer'),
+  cache: config.redis,
+  accessToken: {
+    key: readFileSync(config.accessToken.privateKeyPath),
+    exp: config.accessToken.expiresIn,
+    alg: config.accessToken.algorithm
+  },
+  refreshToken: {
+    key: config.refreshToken.secret,
+    exp: config.refreshToken.expiresIn,
+    alg: config.refreshToken.algorithm
+  }
+});
+
+auth.use('basic', req => {
+  return User.findOne({ username: req.body.username })
+    .then(user => Promise.all([user, user.verifyPassword(req.body.password)]))
+    .then(userMatch => {
+      const user = userMatch[0];
+      const isMatch = userMatch[1];
+
+      if (!isMatch) throw 'Unauthorized: username or password do not match.';
+
+      return user;
+    })
+    .then(user => user.tokenPayload());
+});
+
+// Authorization
+const authorizedRefresh = cred.authorize({
+  issuer: app.get('issuer'),
+  key: config.refreshToken.secret,
+  alg: config.refresh.algorithm
+});
+const authorizedAccess = cred.authorize({
+  name: app.get('name'),
+  issuer: app.get('issuer'),
+  key: readFileSync(config.accessToken.publicKeyPath),
+  alg: config.accessToken.algorithm
+});
 
 // Logs
 if (process.env.NODE_ENV === ('development' || 'test')) {
@@ -35,7 +82,6 @@ const authRoutes = require('./routes/auth_routes');
 const publicRoutes = require('./routes/public_routes');
 const userRoutes = require('./routes/user_routes');
 const resourceRoutes = require('./routes/resource_routes');
-const { requireValidToken } = require('cred');
 
 // Unauthenticated routes
 app.use('/', [
@@ -45,7 +91,7 @@ app.use('/', [
 
 // All API routes require a valid token.
 app.use('/', [
-  requireAccessToken,
+  cred.requireAccessToken,
   userRoutes,
   resourceRoutes
 ]);
