b6602539c279369744755beb3e6310ed961aaf9b
slode <rob.m@myplanet.com>
2016-09-20 15:04:03 -0400

    remove express-status-monitor

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const RateLimit = require('express-rate-limit')
-const statusMonitor = require('express-status-monitor')
 const morgan = require('morgan')
 const cred = require('./cred')
 const config = require('../config/server')
@@ -28,8 +27,14 @@ app.use(cors({
   credentials: false
 }))
 
+// Pass remote address through proxy so that limiter knows about it.
+app.enable("trust proxy")
+
+// Disable X-Powered-By header.
+app.disable('x-powered-by')
+
 // Serve static files from /build in development (on the production server,
-// this would be handled by a web server/proxy like nginx).
+// this would be handled by a web server/proxy, like nginx).
 if (process.env.NODE_ENV === ('development' || 'test')) {
   app.use('/', express.static(`${ __dirname }/../build`))
 }
@@ -51,16 +56,10 @@ const limiter = new RateLimit({
   }
 })
 
-// Pass remote address through proxy so that limiter knows about it.
-app.enable("trust proxy")
-
 // Apply limiter globally.
 // Alternatively, we could create different limiters and apply them on specific
 // routes like `app.use('/my-route', limiter)`.
-//app.use(limiter)
-
-// Disable X-Powered-By header.
-app.disable('x-powered-by')
+app.use(limiter)
 
 // Logs
 // ----
@@ -74,49 +73,6 @@ const tokenRoutes = require('./routes/token_routes')
 const publicRoutes = require('./routes/public_routes')
 const userRoutes = require('./routes/user_routes')
 const resourceRoutes = require('./routes/resource_routes')
-
-// Status Monitor
-// app.use('/status', [
-//   cred.requireAccessToken,
-//   cred.requireProp('isActive', true),
-//   statusMonitor({
-//     path: '/status',
-//     socketPort: 41338,
-//     spans: [
-//       {
-//         interval: 1, // every second
-//         retention: 60 // keep 60 datapoints in memory
-//       },
-//       {
-//         interval: 5, // every 5 seconds
-//         retention: 60
-//       },
-//       {
-//         interval: 15, // every 15 seconds
-//         retention: 60
-//       }
-//     ]
-//   })
-// ])
-
-// app.use(statusMonitor({
-//   path: '/status',
-//   socketPort: 41338,
-//   spans: [
-//     {
-//       interval: 1, // every second
-//       retention: 60 // keep 60 datapoints in memory
-//     },
-//     {
-//       interval: 5, // every 5 seconds
-//       retention: 60
-//     },
-//     {
-//       interval: 15, // every 15 seconds
-//       retention: 60
-//     }
-//   ]
-// }))
 
 // Unauthenticated routes
 app.use('/', [
