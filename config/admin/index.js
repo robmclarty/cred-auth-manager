cc8173f900beaac4644bd79bf2d6a289f21dce9c
slode <rob.m@myplanet.com>
2017-03-10 10:50:57 -0500

    update how front-end config is loaded based on ENV

--- config/admin/index.js
+++ config/admin/index.js
@@ -1,15 +1,11 @@
 'use strict'
 
-const isProduction = process.env.NODE_ENV === 'production'
-const settings = isProduction ?
-  require('./settings.json') :
-  require('./settings.sample.json')
+const env = process.env.NODE_ENV || 'development'
 
-const stuff = {
-  appName: process.env.APP_NAME || isProduction ? settings.appName : "cred-auth-manager",
-  authRoot: process.env.AUTH_ROOT || isProduction ? settings.authRoot : "http://localhost:3000"
+const config = {
+  development: require('./development.json'),
+  production: require('./production.json'),
+  staging: require('./staging.json')
 }
 
-console.log('stuff: ', stuff)
-
-module.exports = stuff
+module.exports = config[env]
