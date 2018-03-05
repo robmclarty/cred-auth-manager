80e62aef8ece9074987d054fa849a0a12a9ac5cd
slode <rob.m@myplanet.com>
2018-03-05 14:07:24 -0500

    add comments on setup function usage

--- bin/setup.js
+++ bin/setup.js
@@ -3,18 +3,21 @@
 const argv = require('minimist')(process.argv.slice(2))
 const fse = require('fs-extra')
 
+// `npx cred-auth-manager setup:mirations ./destination/path`
 const setupMigrations = argv => {
   fse.copy('./db/migrations', argv._[1])
     .then(() => console.log('migration files copyied'))
     .catch(err => console.log('ERROR: ', err))
 }
 
+// `npx cred-auth-manager setup:seeders ./destination/path`
 const setupSeeders = argv => {
   fse.copy('./db/seeders', argv._[1])
     .then(() => console.log('seeder files copyied'))
     .catch(err => console.log('ERROR: ', err))
 }
 
+// `npx cred-auth-manager setup:database ./destination/path`
 const setupDatabase = argv => {
   fse.copy('./config/database.js', argv._[1])
     .then(() => console.log('database config copyied'))
