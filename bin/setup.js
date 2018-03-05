a825c1eb967d5e6f1e7fd10cfd432f00845cbf41
slode <rob.m@myplanet.com>
2018-03-05 14:19:32 -0500

    use reference to setup folders relative to location of setup.js script file

--- bin/setup.js
+++ bin/setup.js
@@ -5,21 +5,27 @@ const fse = require('fs-extra')
 
 // `npx cred-auth-manager setup:mirations ./destination/path`
 const setupMigrations = argv => {
-  fse.copy('./db/migrations', argv._[1])
+  const migrationsFolder = `${ __dirname }/../db/migrations`
+
+  fse.copy(migrationsFolder, argv._[1])
     .then(() => console.log('migration files copyied'))
     .catch(err => console.log('ERROR: ', err))
 }
 
 // `npx cred-auth-manager setup:seeders ./destination/path`
 const setupSeeders = argv => {
-  fse.copy('./db/seeders', argv._[1])
+  const seedersFolder = `${ __dirname }/../db/seeders`
+
+  fse.copy(seedersFolder, argv._[1])
     .then(() => console.log('seeder files copyied'))
     .catch(err => console.log('ERROR: ', err))
 }
 
 // `npx cred-auth-manager setup:database ./destination/path`
 const setupDatabase = argv => {
-  fse.copy('./config/database.js', argv._[1])
+  const databaseConfig = `${ __dirname }/../config/database.js`
+
+  fse.copy(databaseConfig, argv._[1])
     .then(() => console.log('database config copyied'))
     .catch(err => console.log('ERROR: ', err))
 }
